cc9b46415ee504e21c7a8739315f4d0a5aacad96
jan <>
2008-12-29 04:13:14 +0000

    * obby/rudel-obby.el (whole file): improved comments   (rudel-obby-backend::rudel-ask-connect-info): removed :override tag;   added comments   (rudel-obby-backend::rudel-connect): removed :override tag; use   `make-network-process' instead of `open-network-stream' and attach   filter and sentinel right away; removed some debug code   (rudel-obby-backend::rudel-disconnect): removed :override tag   (rudel-obby-backend::rudel-subscribe-to): removed :override tag   (rudel-obby-backend::rudel-local-insert): removed :override tag   (rudel-obby-backend::rudel-local-delete): removed :override tag   (rudel-obby-backend::rudel-make-document): new method; creates a new   rudel-obby-document object   (rudel-obby-backend::rudel-available-document-id): obtains an	unused   document id, which can be assigned to a new document   (class rudel-obby-connection): removed useless `host' and `port'   slots   (rudel-obby-connection::rudel-receive): removed some debug code   (rudel-obby-connection::rudel-send): removed some debug code   (rudel-obby-connection::rudel-obby/net6_client_join): fixed syntax   error   (class rudel-obby-user): added accessors for slots `client-id' and   `user-id'   (rudel-obby-user::eieio-speedbar-description): removed :override tag   (rudel-obby-user::eieio-speedbar-object-buttonname): removed   :override tag   (class rudel-obby-document): added accessors and documentation for   slot `id'   (rudel-obby-document::eieio-speedbar-object-buttonname): removed   :override tag   (rudel-obby-replace-in-string): new function; replace a set of   patterns in a string   (rudel-obby-escape-string): new function; replace obby control   characters with their escape sequences   (rudel-obby-unescape-string): new function; invers of   `rudel-obby-escape-string'   (rudel-obby-parse-color): added documentation   (rudel-obby-format-color): added documentation   (rudel-obby-assemble-message): properly escape message components   (rudel-obby-parse-message): properly unescape message components

--- obby/rudel-obby.el
+++ obby/rudel-obby.el
@@ -1,111 +1,128 @@
-;;; rudel-obby.el --- 
+;;; rudel-obby.el --- An obby backend for Rudel
 ;;
 ;; Copyright (C) 2008 Jan Moringen
 ;;
 ;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+;; Keywords: Rudel, obby, backend, implementation
+;; X-RCS: $Id:$
 ;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 3, or (at
 ;; your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ;; General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING. If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; along with this program; see the file COPYING. If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA, or see <http://www.gnu.org/licenses>.
 
 ;;; Commentary:
 ;;
-;; 
+;; This file contains a Rudel backend, which implements the obby
+;; protocol (used by the Gobby collaborative editor).
+
+;;; History:
+;;
+;; 0.1 - Initial version
 
 ;;; Code:
-(require 'eieio)
-
-(require 'rudel)
+;;
 
 (eval-when-compile
   (require 'cl))
 
+(require 'eieio)
+
+(require 'rudel)
+
 
-;;;  Class rudel-obby-backend
+;;; Class rudel-obby-backend
 ;;
 
 (defclass rudel-obby-backend (rudel-backend)
   ((capabilities :initform '(join host change-color)))
   "Class rudel-obby-backend ")
 
-(defmethod rudel-ask-connect-info :override ((this rudel-obby-backend))
+(defmethod rudel-ask-connect-info ((this rudel-obby-backend))
   ""
+  ;; Read server host and port.
   (let ((host     (read-string "Server: "))
 	(port     (read-number "Port: " 6522))
+	;; Read desired username and color
 	(username (read-string "Username: " user-login-name))
 	(color    (read-color  "Color: " 't))) ; TODO default/customize color
-    (list :host host :port port :username username :color color)))
+    (list :host host :port port :username username :color color))
+  )
 
-(defmethod rudel-connect :override ((this rudel-obby-backend) info)
+(defmethod rudel-connect ((this rudel-obby-backend) info)
   ""
-  (let* ((host       (plist-get info :host))
+  (let* ((session    (plist-get info :session))
+	 (host       (plist-get info :host))
 	 (port       (plist-get info :port))
-	 (socket     (open-network-stream host
-					  (format "*obby-%s*" host)
-					  host
-					  port))
+	 ;; Create the network process
+	 (socket     (make-network-process
+		      :name     host
+		      :host     host
+		      :service  port
+		      ;; Install connection filter to redirect data to
+		      ;; the connection object
+		      :filter   'rudel-filter-dispatch
+		      ;; Install connection sentinel to redirect state
+		      ;; changes to the connection object
+		      :sentinel 'rudel-sentinel-dispatch))
 	 (connection (rudel-obby-connection host
-					    :host   host
-					    :port   port
-					    :socket socket
-					    :info   info)))
-    (put (intern (process-name socket)) 
-	 :connection connection)
-    (set-process-filter socket
-			(lambda (process data)
-			  (let ((connection (get (intern (process-name process))
-						 :connection)))
-			    (rudel-receive connection data))))
+                      :socket socket
+		      :info   info)))
+    connection)
+  )
 
-    (with-current-buffer "*obby-localhost*"
-      (erase-buffer)
-      (goto-char 0))
+(defmethod rudel-make-document ((this rudel-obby-backend)
+				name session)
+  ""
+  ;; Find an unused document id and create a document with that id.
+  (let ((id (rudel-available-document-id this session)))
+    (with-slots (user-id) (oref session :self)
+      (rudel-obby-document name 
+			   :session  session
+			   :id       id
+			   :owner-id user-id)))
+  )
 
-    (with-current-buffer "SESSION"
-      (erase-buffer)
-      (goto-char 0))
-
-    connection))
+(defmethod rudel-available-document-id ((this rudel-obby-backend)
+					session)
+  "Return a document id, which is not in use in SESSION."
+  ;; Look through some candidates until an unused id is hit.
+  (let* ((used-ids (with-slots (documents) session
+		     (mapcar 'rudel-id documents)))
+	 (test-ids (number-sequence 0 (length used-ids))))
+    (car (sort (set-difference test-ids used-ids) '<)))
+  )
 
 
 ;;; Class rudel-obby-connection
 ;;
 
 (defclass rudel-obby-connection (rudel-connection)
-  ((host   :initarg :host
-	   :type    string
-	   :documentation
-	   "")
-   (port   :initarg :port
-	   :type    (integer 0 65535)
-	   :documentation
-	   "")
-   (socket :initarg :socket
+  ((socket :initarg :socket
 	   :documentation
 	   "")
    (info   :initarg :info
-	   :type list ;plist
+	   :type    list
 	   :documentation
 	   "Stores connection information for later use."))
   "Class rudel-obby-connection ")
 
-(defmethod rudel-disconnect :override ((this rudel-obby-connection))
+(defmethod rudel-disconnect ((this rudel-obby-connection))
   ""
   (with-slots (socket) this
     (delete-process socket)))
 
-(defmethod rudel-subscribe-to :override ((this rudel-obby-connection) document)
+(defmethod rudel-subscribe-to ((this rudel-obby-connection) document)
   ""
   (let* ((session (oref this :session))
 	 (user-id (oref (oref session :self) :user-id)))
@@ -117,7 +134,7 @@
       (setq subscribed 't)))
   )
 
-(defmethod rudel-local-insert :override ((this rudel-obby-connection) document from to what)
+(defmethod rudel-local-insert ((this rudel-obby-connection) document from to what)
   ""
   (with-slots (owner-id (doc-id :id) (local-revision :revision)) document
     (let ((remote-revision 0))
@@ -133,25 +150,12 @@
     (incf local-revision))
   )
 
-(defmethod rudel-local-delete :override ((this rudel-obby-connection))
+(defmethod rudel-local-delete ((this rudel-obby-connection))
   ""
   )
 
-(defmethod rudel-receive ((this rudel-obby-connection) data) ; TODO override?
+(defmethod rudel-receive ((this rudel-obby-connection) data)
   ""
-  (with-current-buffer "*obby-localhost*"
-    (goto-char 0)
-    (insert (concat "> " data)))
-    
-
-  (require 'data-debug)
-  (save-excursion
-    (unless (get-buffer "SESSION")
-      (data-debug-new-buffer "SESSION"))
-    (set-buffer "SESSION")
-    (goto-char (point-min))
-    (data-debug-insert-thing (oref this :session) "# " ""))
-
   (let* ((lines    (split-string data "\n" 't))
 	 (messages (mapcar 'rudel-obby-parse-message lines))) ; TODO does not work when partial message arrives
     ;; Dispatch messages to respective handlers
@@ -169,11 +173,6 @@
   (let ((socket  (oref this :socket))
 	(message (apply 'rudel-obby-assemble-message
 			(cons name arguments))))
-
-    (with-current-buffer "*obby-localhost*"
-      (goto-char 0)
-      (insert (concat "< " message)))
-    
     (process-send-string socket message))
   )
 
@@ -208,7 +207,8 @@
       (rudel-add-user session user)
       (unless (slot-boundp session :self)
 	(oset session :self user))))
-  (message "Client joined: %s %s" name (rudel-obby-parse-color color)))
+  (message "Client joined: %s %s" name (rudel-obby-parse-color color))
+  )
 
 (defmethod rudel-obby/net6_client_part ((this rudel-obby-connection) client-id)
   ""
@@ -345,29 +345,31 @@
   )
 
 
-;;; Obby user
+;;; Class rudel-obby-user
 ;;
 
 (defclass rudel-obby-user (rudel-user)
-  ((client-id  :initarg :client-id
-	       :type    integer
+  ((client-id  :initarg  :client-id
+	       :type     integer
+	       :accessor rudel-client-id
 	       :documentation
 	       "")
-   (user-id    :initarg :user-id
-	       :type    integer
+   (user-id    :initarg  :user-id
+	       :type     integer
+	       :accessor rudel-id
 	       :documentation
 	       "")
-   (connected  :initarg :connected
-	       :type    boolean
+   (connected  :initarg  :connected
+	       :type     boolean
 	       :documentation
 	       "")
-   (encryption :initarg :encryption
-	       :type    boolean
+   (encryption :initarg  :encryption
+	       :type     boolean
 	       :documentation
 	       ""))
   "Class rudel-obby-user ")
 
-(defmethod eieio-speedbar-description :override ((this rudel-obby-user))
+(defmethod eieio-speedbar-description ((this rudel-obby-user))
   "Provide a speedbar description for THIS."
   (let ((connected  (oref this :connected))
 	(encryption (if (slot-boundp this :encryption)
@@ -378,7 +380,7 @@
 	    (if encryption "Encryption" "Plain")))
   )
 
-(defmethod eieio-speedbar-object-buttonname :override ((this rudel-obby-user))
+(defmethod eieio-speedbar-object-buttonname ((this rudel-obby-user))
   "Return a string to use as a speedbar button for THIS."
   (let ((connected  (oref this :connected))
 	(encryption (if (slot-boundp this :encryption)
@@ -390,14 +392,17 @@
   )
 
 
-;;; Obby document
+;;; Class rudel-obby-document
 ;;
 
 (defclass rudel-obby-document (rudel-document)
   ((id         :initarg  :id
 	       :type     integer
+	       :accessor rudel-id
 	       :documentation
-	       "")
+	       "The id of this document.
+The id has to be unique only with respect to the other documents
+owned by the owner.")
    (owner-id   :initarg  :owner-id
 	       :type     integer
 	       :documentation
@@ -414,46 +419,85 @@
 	       ""))
   "Class rudel-obby-document ")
 
-(defmethod eieio-speedbar-description ((this rudel-document))
+(defmethod eieio-speedbar-description ((this rudel-obby-document))
   "Construct a description for from the name of document object THIS."
   (format "Document %s" (object-name-string this)))
 
-(defmethod eieio-speedbar-object-buttonname :override ((this rudel-obby-document))
+(defmethod eieio-speedbar-object-buttonname ((this rudel-obby-document))
   "Return a string to use as a speedbar button for OBJECT."
   (with-slots (subscribed) this
-    (format "%-12s %s" (object-name-string this) 
+    (format "%-12s %s" (object-name-string this)
 	    (if subscribed "s" "-")))
   )
 
 
-;;; Obby messages
+;;; Obby message functions
 ;;
 
-(defun rudel-obby-assemble-message (name &rest arguments)
-  ""
-  (concat (mapconcat 'identity (cons name arguments) ":") "\n"))
+(defun rudel-obby-replace-in-string (string replacements)
+  "Replace elements of REPLACEMENTS in STRING.
+REPLACEMENTS is a list of conses whose car is the pattern and
+whose cdr is the replacement for the pattern."
+  (let ((result string))
+    (dolist (replacement replacements)
+      (let ((from (car replacement))
+	    (to   (cdr replacement)))
+	(setq result (replace-regexp-in-string
+		      from to result nil 't))))
+    result)
+  )
 
-(defun rudel-obby-parse-message (message)
-  ""
-  (split-string message ":"))
+(defun rudel-obby-escape-string (string)
+  "Replace meta characters in STRING with their escape sequences."
+  (obby-replace-in-string 
+   string 
+   '(("\\\\" . "\\b") ("\n" . "\\n") (":" . "\\d")))
+  )
+
+(defun rudel-obby-unescape-string (string)
+  "Replace escaped versions of obby meta characters in STRING with the actual meta characters."
+  (obby-replace-in-string 
+   string 
+   '(("\\\\n" . "\n") ("\\\\d" . ":") ("\\\\b" . "\\")))
+  )
 
 (defun rudel-obby-parse-color (color)
-  ""
+  "Parse the obby color string COLOR into an Emacs color."
   (let* ((color-numeric (string-to-number color 16))
 	 (color-string  (format "#%04X%04X%04X"
-				(lsh (logand #xff0000 color-numeric) -08) 
+				(lsh (logand #xff0000 color-numeric) -08)
 				(lsh (logand #x00ff00 color-numeric) -00)
 				(lsh (logand #x0000ff color-numeric)  08))))
-    color-string))
+    color-string)
+  )
   
 (defun rudel-obby-format-color (color)
-  ""
+  "Format the Emacs color COLOR as obby color string."
   (let ((color-numeric-r (string-to-number (substring color 1 5) 16))
 	(color-numeric-g (string-to-number (substring color 5 9) 16))
 	(color-numeric-b (string-to-number (substring color 9 13) 16)))
     (format "%02x%02x%02x" (lsh color-numeric-r -8)
 	    (lsh color-numeric-g -8)
-	    (lsh color-numeric-b -8))))
+	    (lsh color-numeric-b -8)))
+  )
+
+(defun rudel-obby-assemble-message (name &rest arguments)
+  ""
+  (concat (mapconcat
+	   (lambda (part)
+	     (if (and (not (null part)) (stringp part))
+		 (rudel-obby-escape-string part)
+	       part))
+	   (cons name arguments) ":")
+	  "\n")
+  )
+
+(defun rudel-obby-parse-message (message)
+  "Split MESSAGE at `:' and unescape resulting parts.
+
+The terminating `\n' should be removed from MESSAGE before
+calling this function."
+  (mapcar 'rudel-obby-unescape-string (split-string message ":")))
 
 
 ;;; Autoloading
