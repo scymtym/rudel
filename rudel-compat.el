bb64dd37404282a0b0cbbcb4d96b88d28d179c05
jan <>
2009-02-05 23:21:32 +0000

    * rudel-compat.el (header): fixed email address, keywords, legal   notice and file commentary

--- rudel-compat.el
+++ rudel-compat.el
@@ -2,24 +2,28 @@
 ;;
 ;; Copyright (C) 2009 Jan Moringen
 ;;
-;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+;; Author: Jan Moringen <scymtym@users.sourceforge.net>
+;; Keywords: Rudel, compatibility
+;; X-RCS: $Id:$
 ;;
-;;  is free software: you can redistribute it and/or modify
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 ;;
-;;  is distributed in the hope that it will be useful,
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with . If not, see <http://www.gnu.org/licenses>.
+;; along with this program. If not, see <http://www.gnu.org/licenses>.
 
 
 ;;; Commentary:
 ;;
+;; This file contains compatibility code required to make Rudel work
+;; with different versions of Emacs.
 
 
 ;;; History:
