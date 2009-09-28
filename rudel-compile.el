6809e4274a13394e3298fea5c5fd7acf84afd83b
scymtym <scymtym@users.sourceforge.net>
2009-09-29 01:13:59 +0200

    * rudel-compile.el (header): added copyright and meta data   (code): let-bind rudel-dir; call `byte-recompile-directory' just   once since it operates recursively

--- rudel-compile.el
+++ rudel-compile.el
@@ -1,9 +1,46 @@
-;; Press M-x eval-buffer to compile and generate autoloads for rudel
+;;; rudel-compile.el --- Byte-compile Rudel
+;;
+;; Copyright (C) 2009 Phil Hagelberg
+;;
+;; Author: Phil Hagelberg <phil@enigma>
+;; Keywords: Rudel, compile
+;; X-RCS: $Id:$
+;;
+;; This file is part of Rudel.
+;;
+;; Rudel is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; Rudel is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Rudel. If not, see <http://www.gnu.org/licenses>.
 
-(setq rudel-dir (file-name-directory
-                 (or (buffer-file-name) load-file-name)))
+
+;;; Commentary:
+;;
+;; Press M-x eval-buffer to byte-compile Rudel.
 
-(dolist (dir '("." "jupiter" "obby" "zeroconf"))
-  (let ((d (concat rudel-dir "/" dir)))
-    (add-to-list 'load-path d)
-    (byte-recompile-directory d 0)))
+
+;;; History:
+;;
+;; 0.1 - Initial revision
+
+
+;;; Code:
+;;
+
+(let ((rudel-dir (file-name-directory
+		  (or (buffer-file-name) load-file-name))))
+  ;; Adjust load path for compilation.
+  (dolist (dir '("." "jupiter" "obby" "zeroconf"))
+    (let ((d (concat rudel-dir "/" dir)))
+      (add-to-list 'load-path d)))
+
+  ;; Byte compile everything.
+  (byte-recompile-directory rudel-dir 0))
