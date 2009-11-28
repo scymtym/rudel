7e94ded904eb2b7cfacf38cfa9e43233883dee97
scymtym <scymtym@users.sourceforge.net>
2009-11-28 05:18:55 +0100

    * rudel-transport.el: socket-like interface for transports   (header): downcased keywords; expanded "Commentary" section   (rudel-transport::rudel-set-filter): renamed   `rudel-transport-set-handler' -> rudel-set-filter   (rudel-transport::rudel-set-sentinel): new method; like   `set-process-sentinel'   (rudel-send): renamed `rudel-transport-send' -> `rudel-send'   (rudel-transport::rudel-start): new method start stopped transport   (rudel-transport-backend::rudel-make-connection): expanded   documentation string

--- rudel-transport.el
+++ rudel-transport.el
@@ -3,29 +3,34 @@
 ;; Copyright (C) 2009 Jan Moringen
 ;;
 ;; Author: Jan Moringen <scymtym@users.sourceforge.net>
-;; Keywords: Rudel, backend, transport
+;; Keywords: rudel, backend, transport
 ;; X-RCS: $Id:$
 ;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
+;; This file is part of Rudel.
+;;
+;; Rudel is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 ;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;; GNU General Public License for more details.
+;; Rudel is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <http://www.gnu.org/licenses>.
+;; along with Rudel. If not, see <http://www.gnu.org/licenses>.
 
 
 ;;; Commentary:
 ;;
-;; This file contains the interface for Rudel transport backends.
+;; This file contains the interface definition for Rudel transport
+;; objects and Rudel transport backends.
 
 
 ;;; History:
+;;
+;; 0.2 - Socket-like interface
 ;;
 ;; 0.1 - Initial revision
 
@@ -45,12 +50,22 @@
   ()
   "Interface for transport objects.")
 
-(defgeneric rudel-transport-send ((this rudel-transport) data)
+(defgeneric rudel-set-filter ((this rudel-transport) handler)
+  "Install HANDLER as dispatcher for messages received by THIS.")
+
+(defgeneric rudel-set-sentinel ((this rudel-transport) handler)
+  "Install HANDLER as dispatcher for state changes of THIS.")
+
+(defgeneric rudel-send ((this rudel-transport) data)
   "Send DATA through THIS transport object.")
 
-(defgeneric rudel-transport-set-handler ((this rudel-transport)
-					 handler)
-  "Install HANDLER as dispatcher for messages received by THIS.")
+(defgeneric rudel-close ((this rudel-transport))
+  "Close THIS.")
+
+;; TODO we could get rid of this of we required implementations to
+;; queue messages until a handler is installed
+(defgeneric rudel-start ((this rudel-transport))
+  "Start THIS.")
 
 
 ;;; Class rudel-transport-backend
@@ -62,10 +77,13 @@
   :abstract t)
 
 (defgeneric rudel-make-connection ((this rudel-transport-backend) info)
-  "Create a transport object according to INFO.")
+  "Create a transport object according to INFO.
+The returned transport object has to be in a stopped state in the
+sense that it does not attempt to dispatch any data to the filter
+function before `rudel-start' has been called.")
 
 (defgeneric rudel-wait-for-connections ((this rudel-transport-backend)
-					info)
+					info) ;; TODO callback?
   "Create a transport object according to INFO.")
 
 (provide 'rudel-transport)
