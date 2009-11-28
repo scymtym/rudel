3c2fb8a0d2cdde06552b784e04f5f5dfa0d2657a
scymtym <scymtym@users.sourceforge.net>
2009-11-28 05:12:00 +0100

    * rudel-transport-util.el: new file; utility functions for   implementing transport functionality * Project.ede (target rudel): added rudel-transport-util.el

---
+++ rudel-transport-util.el
@@ -0,0 +1,39 @@
+;;; rudel-transport-util.el --- Utility functions for Rudel transport functionality
+;;
+;; Copyright (C) 2009 Jan Moringen
+;;
+;; Author: Jan Moringen <scymtym@users.sourceforge.net>
+;; Keywords: rudel, backend, transport, utility, miscellaneous
+;; X-RCS: $Id:$
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses>.
+
+
+;;; Commentary:
+;;
+;; This file contains utility functions for implementing Rudel
+;; transport functionality.
+
+
+;;; History:
+;;
+;; 0.1 - Initial revision
+
+
+;;; Code:
+;;
+
+
+(provide 'rudel-transport-util)
+;;; rudel-transport-util.el ends here
