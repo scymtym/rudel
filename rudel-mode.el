beca8308404d754540252a04454356dba8ce3369
Joe Corneli <holtzermann17@gmail.com>
2010-07-25 22:50:32 +0200

    Renamed rudel-unpublish-buffer to rudel-unsubscribe * rudel.el (rudel-attach-to-buffer): changed   rudel-unpublish-buffer to rudel-unsubscribe   (rudel-detach-from-buffer): likewise   (rudel-unsubscribe): likewise * rudel-mode.el (rudel-minor-keymap): likewise * rudel-interactive.el (rudel-allocate-buffer-clear-existing):   likewise

--- rudel-mode.el
+++ rudel-mode.el
@@ -556,7 +556,7 @@ line publish state mode; otherwise, turn it off."
       [ "Publish current Buffer"   rudel-publish-buffer
 	                           (and rudel-current-session
 					(not (rudel-buffer-has-document-p))) ]
-      [ "Unpublish current Buffer" rudel-unpublish-buffer
+      [ "Unsubscribe from current Buffer" rudel-unpublish-buffer
 	                           (rudel-buffer-has-document-p) ]
       [ "Subscribe to Document"    rudel-subscribe
 	                           rudel-current-session ]
